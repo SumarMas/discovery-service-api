53f01f5a6426cbc93ca3b8f516200f6a16665b59
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2025-09-20 19:01:18 -0300

    Update DiscoveryServiceApplication.java

--- src/main/java/com/platform/discovery_service/DiscoveryServiceApplication.java
+++ src/main/java/com/platform/discovery_service/DiscoveryServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main class for the Discovery Service application.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
@@ -11,7 +14,7 @@ public class DiscoveryServiceApplication {
      * Main method to run the Spring Boot application.
      * @param args command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
